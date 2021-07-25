f4fae326b39da1474bf85b98d5ee4a210712772d
Lucas-Peng-CS <cs20210310@gmail.com>
2021-07-25 22:52:03 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 const port = 3000
-
 const app = express()
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 app.use(methodOverride('_method'))
 app.use(routes);
