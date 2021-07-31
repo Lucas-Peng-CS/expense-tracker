fbaddddc1708430032261c8325749cc310d17c0d
Lucas-Peng-CS <cs20210310@gmail.com>
2021-07-31 09:57:38 +0800

    feat: add update expense function

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -4,9 +4,16 @@ const Record = require('../../models/record')
 const Category = require("../../models/category");
 
 router.get('/new', (req, res) => {
+  const date = new Date()
+  const currentDate = formatDate(date);
+  function formatDate (date) {
+    let month = date.getMonth() + 1
+    month = month < 10 ? '0' + month : month;
+    return date.getFullYear() + "-" + month + "-" + date.getDate()
+  }
   Category.find()
     .lean()
-    .then((categories) => res.render("new", { categories }))
+    .then((categories) => res.render("new", { categories, currentDate }))
     .catch((error) => console.error(error));
 })
 router.post('/', (req, res) => {
@@ -14,13 +21,29 @@ router.post('/', (req, res) => {
   return Record.create(data)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
-  
 })
 router.get('/:id/edit', (req, res) => {
-  
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => {
+      Category.find()
+        .lean()
+        .then((categories) =>
+          res.render("edit", { record, categories })
+        );
+    })
+    .catch((error) => console.log(error));
 })
 router.put('/:id', (req, res) => {
-
+  const id = req.params.id
+  return Record.findById(id)
+    .then((record) => {
+      record = Object.assign(record, req.body)
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error));
 })
 router.delete('/:id', (req, res) => {
 
